a505b9d4cf49e97eb1f3918f81372480b668eec9
Lev Ustimenko <49372672+Leevandr@users.noreply.github.com>
2024-07-07 22:08:15 +0300

    Delete scriptloxxxx.js

--- scriptloxxxx.js
+++
@@ -1,8 +0,0 @@
-var ws = new WebSocket('ws://79.139.188.58:1234');
-ws.onopen = function() {
-    ws.send('Connected');
-};
-ws.onmessage = function(event) {
-    var command = prompt("Enter command to execute:");
-    ws.send(command);
-};
